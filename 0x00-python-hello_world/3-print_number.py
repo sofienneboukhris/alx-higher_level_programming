eb3b7856e12c7b765e26dddf9f8810bf4ece8758
sofienneboukhris <sofienneboukhris@gmail.com>
2022-09-19 13:31:04 +0100

    first commit

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 number = 98
-print ("%s. Battery street")[D % number[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[[C[C[C[C[C
+"%s Battery street" % number
